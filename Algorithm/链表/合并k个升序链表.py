9c651a3187d956a6347f99ea962c1d29f6d0d935
profighted <1412822254@qq.com>
2025-12-05 11:57:49 +0800

    链表

---
+++ Algorithm/链表/合并k个升序链表.py
@@ -0,0 +1,27 @@
+from typing import List
+from typing import Optional
+from heapq import heapify, heappop, heappush
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+ListNode.__lt__=lambda a, b : a.val< b.val
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        cur=dummy=ListNode()
+        h=[head for head in lists if head]
+        heapify(h)
+
+        while h:
+            node=heappop(h)
+            if node.next:
+                heappush(h,node.next)
+
+            cur.next=node
+            cur=cur.next
+        return dummy.next
+            
